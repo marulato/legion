bd493baa4441635ef933d29a8f678c49cb73a34f
35197 <351975711@qq.com>
2020-07-26 22:54:49 +0800

    quartz tables

--- src/test/java/org/zenith/legion/LegionApplicationTests.java
+++ src/test/java/org/zenith/legion/LegionApplicationTests.java
@@ -1,16 +1,28 @@
 package org.zenith.legion;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zenith.legion.common.persistant.SimpleSQLGenerator;
+import org.zenith.legion.sysadmin.entity.BatchJob;
+import org.zenith.legion.sysadmin.entity.BatchJobStatus;
+import org.zenith.legion.sysadmin.service.BatchJobConfigurationService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class LegionApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
 
     }
+
+    public static void main(String[] args) throws Exception {
+        SimpleSQLGenerator simpleSQLGenerator = new SimpleSQLGenerator();
+        System.out.println(simpleSQLGenerator.insert(new BatchJobStatus()));
+    }
+
 
 }
